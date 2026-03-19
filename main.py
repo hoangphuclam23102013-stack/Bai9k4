780460084f2ca3215b2c4bb6d4d00b40be782e4f
hoangphuclam23102013-stack <hoangphuclam23102013@gmail.com>
2026-03-19 18:47:41 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+
+import streamlit as st
+st.set_page_config(page_title="Thu vien dong vat", page_icon=":question:", layout='wide')
+st.title('Thu vien dong vat')
+st.write('Hay chon 1 con vat, toi se hien thi thong tin con vat')
+Tinh_cach = {
+    'con meo': ['https://tiengdong.com/tieng-meo-duc-keu', 'https://hoanghamobile.com/tin-tuc/wp-content/uploads/2024/08/anh-con-meo-cute.jpg', 'https://www.tiktok.com/@3conmeota/video/7503908811447471367'],
+    'con cho':['https://tiengdong.com/tieng-cho-sua-kinh-hoang', 'https://img.tripi.vn/cdn-cgi/image/width=700,height=700/https://gcs.tripi.vn/public-tripi/tripi-feed/img/482759ycb/anh-mo-ta.png', 'https://www.youtube.com/watch?v=GuS4-F7zGkI']}
+cols = st.columns(len(Tinh_cach))
+chon = None
+for i, (con_vat, tinh_cach) in enumerate(Tinh_cach.items()):
+    with cols[i]:
+        if cols[i].button(con_vat):
+            chon = con_vat
+if chon:
+    with st.expander(chon):
+         st.write('Am thanh: ')
+         st.audio(Tinh_cach[chon][0])
+         st.write('Hinh anh: ')
+         st.write(Tinh_cach[chon][1])
+         st.write('Video: ')
+         st.video(Tinh_cach[chon][2])
